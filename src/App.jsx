6fce5be05152422dd4296d516c6728474df2b99f
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-02-02 11:00:32 +0200

    Add the markup and the CSS styles of the input element which takes in the initial bill amount from the user

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,23 @@ import React from "react";
 function App() {
     return (
         <div className="container">
-            <div>THE TIP CALCULATOR</div>
+            <div className="grid calculator">
+                <form className="calculator__input-box">
+                    <label
+                        className="calculator__bill"
+                        htmlFor="bill"
+                    >
+                        bill
+                    </label>
+                    <input
+                        type="text"
+                        className="calculator__bill-input"
+                        placeholder="0"
+                        id="bill"
+                    />
+                </form>
+                <div className="calculator__output-box">SECOND</div>
+            </div>
         </div>
     );
 }
