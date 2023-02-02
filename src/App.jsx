bce7ed14959f510bd3c46939dae9bffe45b4453c
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-02-02 13:19:26 +0200

    Add the buttons and the input element for selecting the tip percentage

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,19 @@ function App() {
             <div className="grid calculator">
                 <form className="calculator__input-box">
                     <InitialBill />
+                    <p className="calculator__tip-percent">select tip %</p>
+                    <div className="calculator__buttons-box">
+                        <button className="calculator__buttons">5%</button>
+                        <button className="calculator__buttons">10%</button>
+                        <button className="calculator__buttons">15%</button>
+                        <button className="calculator__buttons">25%</button>
+                        <button className="calculator__buttons">50%</button>
+                        <input
+                            type="text"
+                            placeholder="custom"
+                            className="calculator__custom"
+                        />
+                    </div>
                 </form>
                 <div className="calculator__output-box">SECOND</div>
             </div>
