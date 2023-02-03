32ec4a10dcd3aad5cbb5d9023f2309ff691009fe
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-02-03 16:18:41 +0200

    Add the markup and CSS styles for the output which displays the total tip amount per person

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,22 @@ function App() {
                     <TipPercentage />
                     <NumPeople />
                 </form>
-                <div className="calculator__output-box">SECOND</div>
+                <div className="calculator__output-box">
+                    <div className="calculator__output-box-1">
+                        <div>
+                            <span className="calculator__tip-amount">
+                                tip amount
+                            </span>
+                            <span className="calculator__per-person">
+                                /&nbsp;&nbsp;person
+                            </span>
+                        </div>
+                        <span className="calculator__amount">
+                            <span>$</span>
+                            <span>0.00</span>
+                        </span>
+                    </div>
+                </div>
             </div>
         </div>
     );
