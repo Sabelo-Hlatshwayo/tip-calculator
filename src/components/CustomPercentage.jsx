d8f2da8f0c2c0869d3559a2d0cf0e528417afa4d
Sabelo Hlatshwayo <evidence.hlatshwayo@gmail.com>
2023-02-05 11:13:55 +0200

    Attach a change event handler on the input field of the CustomPercent component in order to make this input field a controlled component

--- src/components/CustomPercentage.jsx
+++ src/components/CustomPercentage.jsx
@@ -1,11 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 
 function CustomPercentage() {
+    const [customTip, setCustomTip] = useState("");
+    const handleCustomTip = (e) => setCustomTip(() => e.target.value);
+
     return (
         <input
             type="text"
             placeholder="custom"
             className="calculator__custom"
+            value={customTip}
+            onChange={handleCustomTip}
         />
     );
 }
